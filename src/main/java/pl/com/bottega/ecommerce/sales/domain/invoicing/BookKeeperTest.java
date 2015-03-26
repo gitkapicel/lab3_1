8860765464331a85484c57ac2a7e1a74e96218e7
Lukasz Tracz <kapicel20wp.pl>
2015-03-26 09:48:07 +0100

    Zmiana nazwy testu 1 i utworzenie jeszcze nie dzialajacego testu 2.

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -17,7 +17,45 @@ import static org.mockito.Mockito.*;
 public class BookKeeperTest {
 
 	@Test
-	public void test() {
+	public void request_one_invoice_should_return_one_invoice() {
+		//given
+		Money money = new Money(10);
+		Id id = new Id("1");
+		TaxPolicy tax =mock(TaxPolicy.class);
+		InvoiceFactory invoiceFactory = mock(InvoiceFactory.class);
+		BookKeeper book = new BookKeeper(invoiceFactory);
+		ClientData clientData = new ClientData(id,"dzemba");
+		InvoiceRequest invoiceRequest = new InvoiceRequest(clientData);
+		ProductData productData = new ProductData(id,money, "book",ProductType.DRUG, new Date());
+		RequestItem requestitem = new RequestItem(productData,15,money);
+		
+	
+		when(invoiceFactory.create(clientData)).thenReturn(new Invoice(id,clientData));
+		
+		when(tax.calculateTax(ProductType.DRUG, money)).thenReturn(new Tax(money,"opis"));
+		
+
+       invoiceRequest.add(requestitem);
+		
+		//when
+       Invoice invoice = book.issuance(invoiceRequest,tax);
+		//when(book.issuance(invoiceRequest, tax).thenReturn());
+		
+		
+		
+		//then
+		
+		assertThat(invoice.getItems().size(),is(1));
+		
+		
+		
+	}
+
+	
+	
+	
+	@Test
+	public void request_one_invoice_should_return_two_invoice() {
 		//given
 		Money money = new Money(10);
 		Id id = new Id("1");

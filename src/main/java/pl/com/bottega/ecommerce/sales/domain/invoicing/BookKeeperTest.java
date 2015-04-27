305ca30268f4a82afd87b3ac9907d9fa0721f9f4
Lukasz Tracz <kapicel20@wp.pl>
2015-04-27 10:21:13 +0200

    Test case 2 - żądanie wydania faktury z dwiema pozycjami powinno wywołać metodę calculateTax dwa razy.

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import java.util.Date;
 
 import org.junit.Test;
+import org.mockito.Mockito;
 import org.hamcrest.Matchers.*;
 
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
@@ -55,38 +56,34 @@ public class BookKeeperTest {
 	
 	
 	@Test
-	public void request_one_invoice_should_return_two_invoice() {
-		//given
-		Money money = new Money(10);
+	public void should_two_invoice_callCalculateTaxTwice() {
+
+		// given
 		Id id = new Id("1");
-		TaxPolicy tax =mock(TaxPolicy.class);
+		Money money = new Money(10);
+		//ProductType productTypeEveryItem = ProductType.FOOD;
+		ClientData clientData = new ClientData(id, "Arek");
+		ProductData productData = new ProductData(id,money, "book",ProductType.DRUG, new Date());
+		RequestItem requestitem = new RequestItem(productData, 4,
+				money);
+
 		InvoiceFactory invoiceFactory = mock(InvoiceFactory.class);
 		BookKeeper book = new BookKeeper(invoiceFactory);
-		ClientData clientData = new ClientData(id,"dzemba");
+		when(invoiceFactory.create(clientData)).thenReturn(
+				new Invoice(id, clientData));
+		TaxPolicy tax = mock(TaxPolicy.class);
+		when(tax.calculateTax(ProductType.DRUG, money))
+				.thenReturn(new Tax(money, "opis"));
+
 		InvoiceRequest invoiceRequest = new InvoiceRequest(clientData);
-		ProductData productData = new ProductData(id,money, "book",ProductType.DRUG, new Date());
-		RequestItem requestitem = new RequestItem(productData,15,money);
-		
-	
-		when(invoiceFactory.create(clientData)).thenReturn(new Invoice(id,clientData));
-		
-		when(tax.calculateTax(ProductType.DRUG, money)).thenReturn(new Tax(money,"opis"));
-		
+		invoiceRequest.add(requestitem);
+		invoiceRequest.add(requestitem);
 
-       invoiceRequest.add(requestitem);
-		
-		//when
-       Invoice invoice = book.issuance(invoiceRequest,tax);
-		//when(book.issuance(invoiceRequest, tax).thenReturn());
-		
-		
-		
-		//then
-		
-		assertThat(invoice.getItems().size(),is(1));
-		
-		
-		
+		// when
+		Invoice invoiceResult = book.issuance(invoiceRequest, tax);
+
+		// then
+		Mockito.verify(tax, Mockito.times(2)).calculateTax(
+				ProductType.DRUG, money);
 	}
-
 }

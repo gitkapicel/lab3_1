8253bd05ef3cbf351811f539d4c4cf1b5dea7a4f
Lukasz Tracz <kapicel20wp.pl>
2015-05-07 18:27:10 +0200

    Refaktoryzacja pierwszego testu z czesci pierwszej.

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -22,31 +22,35 @@ public class BookKeeperTest {
 		//given
 		Money money = new Money(10);
 		Id id = new Id("1");
-		TaxPolicy tax =mock(TaxPolicy.class);
 		InvoiceFactory invoiceFactory = mock(InvoiceFactory.class);
 		BookKeeper book = new BookKeeper(invoiceFactory);
 		ClientData clientData = new ClientData(id,"dzemba");
+				
+		
+		TaxPolicy taxPolicy = mock(TaxPolicy.class);
+		
+		
+		when(invoiceFactory.create(clientData)).thenReturn(new Invoice(id, clientData));
+	    
+		
 		InvoiceRequest invoiceRequest = new InvoiceRequest(clientData);
-		ProductData productData = new ProductData(id,money, "book",ProductType.DRUG, new Date());
-		RequestItem requestitem = new RequestItem(productData,15,money);
 		
-	
-		when(invoiceFactory.create(clientData)).thenReturn(new Invoice(id,clientData));
 		
-		when(tax.calculateTax(ProductType.DRUG, money)).thenReturn(new Tax(money,"opis"));
-		
+		when(taxPolicy.calculateTax(ProductType.FOOD, money)).thenReturn(
+				new Tax(money, "opis"));
 
-       invoiceRequest.add(requestitem);
+		ProductData productData = new ProductData(id, money, "book",
+				ProductType.FOOD, new Date(0));
+
+		RequestItem requestItem = new RequestItem(productData, 20, money);
+		invoiceRequest.add(requestItem);
 		
-		//when
-       Invoice invoice = book.issuance(invoiceRequest,tax);
-		//when(book.issuance(invoiceRequest, tax).thenReturn());
-		
-		
-		
-		//then
-		
+		Invoice invoice = book.issuance(invoiceRequest,taxPolicy);
 		assertThat(invoice.getItems().size(),is(1));
+		
+		
+		
+		
 		
 		
 		
